d4ef065149045d50add7fa5313159d2a3900e631
Olivera-UX <65922397+Olivera-UX@users.noreply.github.com>
2020-11-05 14:16:17 -0500

    update location
    new

---
+++ js/index.js
@@ -0,0 +1,17 @@
+    $(document).ready(
+        function(){
+            $("#first").fadeIn()
+        }
+        )
+
+// $( ".inlineBlock3" ).hover(function() {
+//     $( this ).append( $( "<span> &#xf118</span>" ) );
+//   }, function() {
+//     $( this ).find( "span" ).last().remove();
+//   }
+// );
+ 
+// $(".inlineBlock3").hover(function() {
+//   $(this).fadeOut( 100 );
+//   $(this).fadeIn( 500 );
+// });
